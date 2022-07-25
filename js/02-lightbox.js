231e00f2ae517fcdd6bcf58290bd293cc4bba97d
KingaStefanska <kinga0stefanska@gmail.com>
2022-07-25 11:24:43 +0200

    Done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector("ul.gallery");
+const pictures = galleryItems
+  .map((element) => {
+    const image = `<a class="gallery__item" href="${element.original}" >
+    <img
+      class="gallery__image"
+      src="${element.preview}"
+      alt="${element.description}"
+      title="${element.description}"
+    />
+  </a>`;
+    return image;
+  })
+  .join(" ");
+
+gallery.insertAdjacentHTML("afterbegin", pictures);
+
+new SimpleLightbox(".gallery a", { captionDelay: 250 });
